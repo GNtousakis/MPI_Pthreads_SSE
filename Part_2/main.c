66a55b81e9cd406e27dff24c090b8d31e23da4e3
GNtousakis <greco1996@gmail.com>
2019-04-13 19:42:04 +0300

    minor changes

--- Part_2/main.c
+++ Part_2/main.c
@@ -11,9 +11,10 @@
 #define MAXSNPS_E 20
 
 double gettime(void);
-float randpval (void);
+float randpval (void);    
 
 //Thead stuff
+#define THREADS 2
 static pthread_t * workerThread; 
 static pthread_barrier_t barrier;
 
@@ -26,76 +27,117 @@ typedef struct
 	int threadOPERATION;
 	
 
-} threadData_t;
+}threadData_t;
 
-	double gettime(void)
-	{
+void initializeThreadData(threadData_t * cur, int i, int threads)
+{
+	cur->threadID=i;
+	cur->threadTOTAL=threads;
+	cur->threadBARRIER=0;
+	cur->threadOPERATION=BUSYWAIT;
+}
+
+double gettime(void)
+{
 	struct timeval ttime;
 	gettimeofday(&ttime , NULL);
 	return ttime.tv_sec + ttime.tv_usec * 0.000001;
-	}
+}
 
-	float randpval (void)
-	{
+float randpval (void)
+{
 	int vr = rand();
 	int vm = rand()%vr;
 	float r = ((float)vm)/(float)vr;
 	assert(r>=0.0f && r<=1.00001f);
 	return r;
-	}
+}
 
-	float max(float a, float b) {
+float max(float a, float b) 
+{
 	if (a>b)
 		return a;
 	return b;
-	}
+}
 
-	float min(float a, float b) {
+float min(float a, float b) 
+{
 	if (a<b)
 		return a;
 	return b;
-	}
-	float sum(float a,float b){
+}
+
+float sum(float a,float b)
+{
 		return(a+b);
+}
+
+	 
+int main(int argc, char ** argv)
+{	
+
+	//Threads
+	int threads = THREADS;	//We pass the number of threads 
+	int s = pthread_barrier_init(&barrier,NULL,(unsigned int)threads);	//We initialize the barrier
+	assert(s == 0);
+
+	workerThread=NULL;
+	workerThread=(pthread *) malloc (sizeof(pthread)*((unsigned long)threads-1)); //We write the workerThread as thread-1 cause the one thread is the master
+
+	threadData* threadData = (threadData *) malloc (sizeof(threadData)*((unsigned long)threads));
+	assert(threadData!=NULL);
+
+	for (int i = 0; i < threads; i++)
+	{
+		initializeThreadData(&threadData[i],i,threads);
 	}
 
-	 int main(int argc, char ** argv)
+	for (int i = 1; i < threads; i++)
 	{
-	 assert(argc==2);
-	 double timeTotalMainStart = gettime();
-	 float avgF = 0.0f;
-	 float maxF = 0.0f;
-	 float minF = FLT_MAX;
-	 unsigned int N = (unsigned int)atoi(argv[1]);
-	 unsigned int iters = 10;
-	 srand(1);
-	 float * mVec = (float*)_mm_malloc(sizeof(float)*N,16);
-	 assert(mVec!=NULL);
-	 float * nVec = (float*)_mm_malloc(sizeof(float)*N,16);
-	 assert(nVec!=NULL);
-	 float * LVec = (float*)_mm_malloc(sizeof(float)*N,16);
-	 assert(LVec!=NULL);
-	 float * RVec = (float*)_mm_malloc(sizeof(float)*N,16);
-	 assert(RVec!=NULL);
-	 float * CVec = (float*)_mm_malloc(sizeof(float)*N,16);
-	 assert(CVec!=NULL);
-	 float * FVec = (float*)_mm_malloc(sizeof(float)*N,16);
-	 assert(FVec!=NULL);
+		pthread_create(&workerThread[i-1],NULL,thread,(void*)(&threadData[i]));
+	}
+	//******************************************************************************************
+	
+	
+
+
+
+
+	assert(argc==2);
+	double timeTotalMainStart = gettime();
+	float avgF = 0.0f;
+	float maxF = 0.0f;
+	float minF = FLT_MAX;
+	unsigned int N = (unsigned int)atoi(argv[1]);
+	unsigned int iters = 10;
+	srand(1);
+	float * mVec = (float*)_mm_malloc(sizeof(float)*N,16);
+	assert(mVec!=NULL);
+	float * nVec = (float*)_mm_malloc(sizeof(float)*N,16);
+	assert(nVec!=NULL);
+	float * LVec = (float*)_mm_malloc(sizeof(float)*N,16);
+	assert(LVec!=NULL);
+	float * RVec = (float*)_mm_malloc(sizeof(float)*N,16);
+	assert(RVec!=NULL);
+	float * CVec = (float*)_mm_malloc(sizeof(float)*N,16);
+	assert(CVec!=NULL);
+	float * FVec = (float*)_mm_malloc(sizeof(float)*N,16);
+	assert(FVec!=NULL);
 
 	for(unsigned int i=0;i<N;i++)
 	{
-	mVec[i] = (float)(MINSNPS_B+rand()%MAXSNPS_E);
-	nVec[i] = (float)(MINSNPS_B+rand()%MAXSNPS_E);
-	LVec[i] = randpval()*mVec[i];
-	RVec[i] = randpval()*nVec[i];
-	CVec[i] = randpval()*mVec[i]*nVec[i];
-	FVec[i] = 0.0;
+		mVec[i] = (float)(MINSNPS_B+rand()%MAXSNPS_E);
+		nVec[i] = (float)(MINSNPS_B+rand()%MAXSNPS_E);
+		LVec[i] = randpval()*mVec[i];
+		RVec[i] = randpval()*nVec[i];
+		CVec[i] = randpval()*mVec[i]*nVec[i];
+		FVec[i] = 0.0;
 
-	assert(mVec[i]>=MINSNPS_B && mVec[i]<=(MINSNPS_B+MAXSNPS_E));
-	assert(nVec[i]>=MINSNPS_B && nVec[i]<=(MINSNPS_B+MAXSNPS_E));
-	assert(LVec[i]>=0.0f && LVec[i]<=1.0f*mVec[i]);
-	assert(RVec[i]>=0.0f && RVec[i]<=1.0f*nVec[i]);
-	assert(CVec[i]>=0.0f && CVec[i]<=1.0f*mVec[i]*nVec[i]);
+		assert(mVec[i]>=MINSNPS_B && mVec[i]<=(MINSNPS_B+MAXSNPS_E));
+		assert(nVec[i]>=MINSNPS_B && nVec[i]<=(MINSNPS_B+MAXSNPS_E));
+		assert(LVec[i]>=0.0f && LVec[i]<=1.0f*mVec[i]);
+		assert(RVec[i]>=0.0f && RVec[i]<=1.0f*nVec[i]);
+		assert(CVec[i]>=0.0f && CVec[i]<=1.0f*mVec[i]*nVec[i]);
 	}
 
 
@@ -112,9 +154,9 @@ typedef struct
 	double timeOmegaTotalStart = gettime();
 	for(unsigned int j=0;j<iters;j++)
 	{
-	avgF = 0.0f;
-	maxF = 0.0f;
-	minF = FLT_MAX;
+		avgF = 0.0f;
+		maxF = 0.0f;
+		minF = FLT_MAX;
 		for(unsigned int i=0;i<N; i+=4)//check this later for any changes!!!!!!!!!!!!!!!!!!!!!!!!!!
 		{
 
@@ -143,21 +185,7 @@ typedef struct
 			minF = (newMin<minF) ? newMin : minF;
 			float sum_all = sum(sum(sum(result[0], result[1]), result[2]), result[3]);
 			avgF+=sum_all;
-
-
-	// float num_0 = LVec[i]+RVec[i];
-	// float num_1 = mVec[i]*(mVec[i]-1.0f)/2.0f;
-	// float num_2 = nVec[i]*(nVec[i]-1.0f)/2.0f;
-	// float num = num_0/(num_1+num_2);
-	// float den_0 = CVec[i]-LVec[i]-RVec[i];
-	// float den_1 = mVec[i]*nVec[i];
-	// float den = den_0/den_1;
-	// FVec[i] = num/(den+0.01f);
-	// maxF = FVec[i]>maxF?FVec[i]:maxF;
-	// minF = FVec[i]<minF?FVec[i]:minF;
-	// avgF += FVec[i];
 		}
-
 	}
 
 	double timeOmegaTotal = gettime()-timeOmegaTotalStart;
