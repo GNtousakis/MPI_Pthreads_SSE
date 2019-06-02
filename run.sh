36192adff04c1a13e1eb896eeaf441e99d70811b
GNtousakis <greco1996@gmail.com>
2019-06-02 13:34:01 +0300

    Finalee

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 gcc ./Part_1/main.c -o ./Part_1/ektelesimo -msse4.2 
 gcc ./Part_2/main.c -o ./Part_2/ektelesimo -msse4.2 -pthread
-gcc ./Part_3/main.c -o ./Part_3/ektelesimo -msse4.2 -pthread
-gcc ./Βοnus_1/main.c -o ./Βοnus_1/ektelesimo -msse4.2 	
+mpicc ./Part_3/main.c -o ./Part_3/ektelesimo -msse4.2 -pthread
 
 sleep_time=3
 sleep_time_between_results=1
@@ -25,11 +24,22 @@ echo -e "------------------------- Second for 4 threads ------------------------
 sleep ${sleep_time}
 
 echo -e "\n\n"
-echo -e "---------------------- We will bonus code --------------------------------------------------"
+echo -e "---------------------- We will SEE + Pthreads + MPI code --------------------------------------------------"
 echo -e "------------------------- to demonstrate that everything works ---------------------------"
 
-./Βοnus_1/ektelesimo 100000000
+echo -e "------------------------- First for 2 processes and 2 threads ---------------------------"
+mpiexec -n 2 ./Part_3/ektelesimo 100000000 2
+sleep ${sleep_time_between_results}
+echo -e "------------------------- Second for 2 processes and 4 threads ---------------------------"
+mpiexec -n 2 ./Part_3/ektelesimo 100000000 4
 sleep ${sleep_time}
+echo -e "------------------------- First for 2 processes and 2 threads ---------------------------"
+mpiexec -n 4 ./Part_3/ektelesimo 100000000 2
+sleep ${sleep_time_between_results}
+echo -e "------------------------- Second for 4 processes and 4 threads ---------------------------"
+mpiexec -n 4 ./Part_3/ektelesimo 100000000 4
+sleep ${sleep_time}
+
 
 
 
